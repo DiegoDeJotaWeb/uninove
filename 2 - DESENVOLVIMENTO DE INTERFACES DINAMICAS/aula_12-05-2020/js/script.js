ec0646c793d84345500cf7549507b6202c0f7a64
DiegoDeJotaWeb <diego.rodrigue.joaquim@gmail.com>
2020-05-12 21:00:46 -0300

    aula_12-05-2020

---
+++ 2 - DESENVOLVIMENTO DE INTERFACES DINAMICAS/aula_12-05-2020/js/script.js
@@ -0,0 +1,10 @@
+//alert('Olá mundo js');
+
+
+
+console.log('escrevendo algo no console do navegador');
+var titulo = document.querySelector("h1");
+
+console.log(titulo);//imprime tudo html + conteudo
+console.log(titulo.textContent);//imprime só o conteudo
+titulo.textContent = "Aparecida Nitrição SA";//modifica o conteudo do h1
